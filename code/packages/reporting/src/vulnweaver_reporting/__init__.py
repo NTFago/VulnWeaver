40eafa403af6a26ffa7d637c6c24727d3fac1af5
Zhang Yuxiang <zhangyuxiang@bupt.edu.cn>
2026-09-09 20:33:25 +0800

    feat(reporting): schedule report jobs

--- code/packages/reporting/src/vulnweaver_reporting/__init__.py
+++ code/packages/reporting/src/vulnweaver_reporting/__init__.py
@@ -5,6 +5,7 @@ from vulnweaver_reporting.html import build_html
 from vulnweaver_reporting.markdown import build_markdown
 from vulnweaver_reporting.pdf import render_pdf
 from vulnweaver_reporting.sarif import build_sarif, validate_sarif
+from vulnweaver_reporting.scheduler import ReportJobScheduler
 
 __all__ = [
     "build_html",
@@ -12,5 +13,6 @@ __all__ = [
     "build_sarif",
     "register_report",
     "render_pdf",
+    "ReportJobScheduler",
     "validate_sarif",
 ]
